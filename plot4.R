e8af83dc773351786ee757a4e0db7731e41e1d9b
Martin Rosellen <martin.rosellen@fu-berlin.de>
2015-09-02 15:20:25 +0200

    comments and a fix

--- plot4.R
+++ plot4.R
@@ -8,20 +8,24 @@ plot4 <- function(){
   timestamp0  <- paste(data0$Date, data0$Time)
   timestamp0  <- strptime(timestamp0, "%Y-%m-%d %H:%M:%S")
   
-  
+  # 2x2 gird of plots
   par(mfrow = c(2,2))
   
+  # uper left
   plot(timestamp0, data0$Global_active_power, type = "l", xlab = "", ylab = "Global active power");
   
+  # upper right
   plot(timestamp0, data0$Voltage, type = "l", xlab = "datetime", ylab = "Voltage");
   
-  
-  
+  # lower left
   plot(timestamp0, data0$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering");
   points(timestamp0, data0$Sub_metering_2, type="l", col = "red")
   points(timestamp0, data0$Sub_metering_3, type="l", col = "blue")
   legend("topright",legend = c("Sub_metering_1" , "Sub_metering_2" , "Sub_metering_3"), lty = 1, col =c("black", "red", "blue"), bty = "n")
   
+  # lower right
   plot(timestamp0, data0$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power");
   
+  #  reset to single plot view (1x1 grid)
+  par(mfrow = c(1,1))
 }
